73038d0fbf6a78f3a786dd534ba86d5625c336ab
Tchore85 <prokopko1@gmail.com>
2019-10-12 20:30:28 -0700

    Update of Navigation pannel, and Fonts

--- script.js
+++ script.js
@@ -9,5 +9,18 @@ document.addEventListener("DOMContentLoaded", function() {
     freeScroll: true
   });
 
+  const emailForm = document.getElementById("mainform");
+  const emailInput = document.getElementById("contact");
+
+  emailForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log(emailInput.value);
+    if (emailInput.value === "") {
+      alert("please give your e-mail adress");
+    } else {
+      alert("thank you for your e-mail");
+    }
+  });
+
   //all of our java code come here
 }); // and of doc ready
